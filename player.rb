953c327823c8bb98438894e849d62ab177e123cc
TeamRory <rorycteam@gmail.com>
2014-06-14 19:14:38 -0400

    The game now has lives and can reset.

--- player.rb
+++ player.rb
@@ -1,13 +1,21 @@
 class Player
+  attr_reader :lives
   def initialize(window)
     @window = window
     @icon = Gosu::Image.new(@window, 'spaceship.png', true)
     @x = 0
     @y = @window.height - @icon.width
+    @explosion = Gosu::Image.new(@window, 'explosion.png', true)
+    @exploded = false
+    @lives = 3
   end
 
   def draw
-    @icon.draw(@x, @y, 1)
+    if @exploded
+      @explosion.draw(@x, @y, 4)
+    else
+      @icon.draw(@x, @y, 1)
+    end
   end
 
   def move_left
@@ -19,8 +27,20 @@ class Player
 
   def move_right
     @x = @x + 10
-    if @x > @window.width - 100
-      @x = @window.width - 100
+    if @x > @window.width - @icon.width
+      @x = @window.width - @icon.width
     end
+  end
+
+  def hit_by?(enemys)
+    @exploded = enemys.any? {|enemy| Gosu::distance(enemy.x + 20, enemy.y + 20, @x + 50, @y + 50) < 50}
+    if @exploded
+      @lives = @lives - 1
+    end
+    @exploded
+  end
+
+  def reset_position
+    @x = rand(@window.width - @icon.width)
   end
 end
--- window.rb
+++ window.rb
@@ -2,21 +2,41 @@ class Window < Gosu::Window
   def initialize
     super(700, 600, false)
     @player = Player.new(self)
-    @enemy = Enemy.new(self)
+    @enemys = [Enemy.new(self), Enemy.new(self)]
+    @enemys = 3.times.map {Enemy.new(self)}
+    @running = true
+    @font = Gosu::Font.new(self, Gosu::default_font_name, 20)
   end
 
   def draw
     @player.draw
-    @enemy.draw
+    @enemys.each {|enemy| enemy.draw}
+    @font.draw("Lives:#{@player.lives}", 10, 10, 3.0, 1.0, 1.0, 0xffffffff)
   end
 
   def update
-    if button_down? Gosu::KbLeft
+    if @running
+      if @player.hit_by?(@enemys)
+        #game ends
+        @running = false
+      else
+        run_game
+      end
+    end
+
+    if @running == false and button_down? Gosu::Button::KbR and @player.lives > 0
+      @running = true
+      @player.reset_position
+    end
+  end
+
+  def run_game
+   if button_down? Gosu::KbLeft
       @player.move_left
     end
     if button_down? Gosu::KbRight
       @player.move_right
     end
-    @enemy.update
+    @enemys.each {|enemy| enemy.update}
   end
 end
